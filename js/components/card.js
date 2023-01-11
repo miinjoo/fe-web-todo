e1b4aae3c596092890f4c8854e1b59778aeb35d9
MyuB <mjycoco0@naver.com>
2023-01-11 12:36:32 +0900

    fix: all btns are changed to direct svg tags

--- js/components/card.js
+++ js/components/card.js
@@ -14,7 +14,20 @@ const cardWrapper = ({ title, text, id }) => {
       <div class="card-header-wrapper">
         <h3 class="card-title">${title}</h3>
         <div class="card-remove-btn-wrapper">
-          <button class="card-remove-btn"></button>
+          <button class="card-remove-btn">
+            <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                  >
+                    <path
+                      d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"
+                      fill="#BDBDBD"
+                    />
+                  </svg>
+          </button>
         </div>
       </div>
       <div class="card-text-wrapper">
@@ -61,8 +74,34 @@ const newColumn = ({ id = "none", title = "냉무" }) => {
         <span class="column-header-num">0</span>
       </div>
       <div class="column-header-right-wrapper">
-        <button class="column-add-btn"></button>
-        <button class="column-remove-btn"></button>
+        <button class="column-add-btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                >
+                  <path
+                    d="M0.105713 7.53033L0.105713 6.46967H6.46967V0.105713H7.53033V6.46967H13.8943V7.53033H7.53033V13.8943H6.46967V7.53033H0.105713Z"
+                    fill="#BDBDBD"
+                  />
+                </svg>
+              </button>
+              <button class="column-remove-btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"
+                    fill="#BDBDBD"
+                  />
+                </svg>
+              </button>
       </div>
     </div>
   </div>
