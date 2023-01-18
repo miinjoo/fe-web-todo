da1920d4a24a4440ce87f711468f009ee7ee409b
MyuB <mjycoco0@naver.com>
2023-01-18 12:25:46 +0900

    feat: created for communicate with json-server

---
+++ js/utils/fetch.js
@@ -0,0 +1,55 @@
+const addCardDataToServer = async (cardData) => {
+  const url = "http://localhost:3000/cards";
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cardData),
+  });
+  return response.ok;
+};
+
+const modifyCardDataInServer = async (cardId, cardData) => {
+  const url = `http://localhost:3000/cards/${cardId}`;
+  const response = await fetch(url, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cardData),
+  });
+  return response.ok;
+};
+
+const getCardDataFromServer = async () => {
+  const url = "http://localhost:3000/cards";
+  const cardDataResponse = await fetch(url, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const cardData = await cardDataResponse.json();
+  return cardData;
+};
+
+const removeCardDataFromServer = async (cardId) => {
+  const url = `http://localhost:3000/cards/${cardId}`;
+  const response = await fetch(url, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = response.json();
+  return data;
+};
+
+export {
+  addCardDataToServer,
+  modifyCardDataInServer,
+  getCardDataFromServer,
+  removeCardDataFromServer,
+};
